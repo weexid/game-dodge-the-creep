99500aa6b3e9d137ee114f70d21c9c5dbef96259
weexid <anemone.darmadi@gmail.com>
2026-04-07 08:24:35 +0800

    fix:main scene

--- main.gd
+++ main.gd
@@ -4,7 +4,7 @@ extends Node
 var score
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	new_game()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -27,3 +27,27 @@ func game_over() -> void:
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()   # Mulai munculin musuh
 	$ScoreTimer.start() # Mulai hitung skor
+
+
+func _on_score_timer_timeout() -> void:
+	score += 1
+
+func _on_mob_timer_timeout() -> void:
+	var mob = mob_scene.instantiate()
+	
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+	
+	mob.position = mob_spawn_location.position
+	
+	var direction = mob_spawn_location.rotation + PI / 2
+	
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	add_child(mob)
+	
+	
